1bd4359575c7e01948e9efad199248f2d62309a9
Ezgi Bas <basezgi@gmail.com>
2022-11-08 11:20:12 -0500

    hi dillon hi richard

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var quote = ["Ambition is the path to success. Persistence is the vehicle you arrive in.", "She who stops being better stops being good.", "Formula for failure: Try to please everybody.", "Keep your eyes on the stars, and your feet on the ground.", "Our greatest glory is not in never falling, but in rising every time we fall.", "Your limitation-it's only your imagination.","Sometimes 'later' becomes 'never'.","Feel the fear and do it anyway.","You can't escape the responsibility of tomorrow by evading it today",];
+var quote = ["Ambition is the path to success. Persistence is the vehicle you arrive in.", "She who stops being better stops being good.", "Formula for failure: Try to please everybody.", "Keep your eyes on the stars, and your feet on the ground.", "Our greatest glory is not in never falling, but in rising every time we fall.", "Your limitation-it's only your imagination.","Sometimes 'later' becomes 'never'.","Feel the fear and do it anyway.","You can't escape the responsibility of tomorrow by evading it today", "hi dillon", "hi richard"];
 var button = $('.button');
 var add = $('.add');
 button.on("click", addQuote);
